7c282e747101c0bec15d1bae1e6a859740bd3b2b
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2018-05-13 01:22:24 +0100

    Made GLSLProvider have a constructor instead, added config interface and init function and now gonna start away on symlinking and linting

--- src/linter/glslProvider.ts
+++ src/linter/glslProvider.ts
@@ -1,12 +1,25 @@
 'use strict';
 
 import * as vscode from 'vscode'
+import * as os from 'os'
+import * as cp from 'child_process'
+import * as sym from 'create-symlink'
+import * as fs from 'fs'
+
+interface config {
+  glslangPath: string
+  tmpdir: string
+}
 
 export default class GLSLProvider implements vscode.CodeActionProvider {
-  private diagnosticCollection?: vscode.DiagnosticCollection
+  private diagnosticCollection: vscode.DiagnosticCollection
+  private config: config
 
-  public activate() {
+  constructor(subs: vscode.Disposable[]) {
     this.diagnosticCollection = vscode.languages.createDiagnosticCollection()
+    
+    subs.push(this)
+    this.config = this.initConfig()
 
     vscode.workspace.onDidOpenTextDocument(this.lint, this)
     vscode.workspace.onDidSaveTextDocument(this.lint, this)
@@ -15,21 +28,32 @@ export default class GLSLProvider implements vscode.CodeActionProvider {
 
     vscode.workspace.onDidChangeConfiguration(this.configChange, this)
 
-    vscode.workspace.textDocuments.forEach(this.lint, this)
+    vscode.workspace.onDidCloseTextDocument((document: vscode.TextDocument) => {
+       this.diagnosticCollection.delete(document.uri)
+    }, null, subs)
+  }
 
-    console.log(vscode.workspace.textDocuments)
+  private initConfig(): config {
+    const c = vscode.workspace.getConfiguration('mcglsl')
+
+    console.log('glslangValidatorPath set to', c.get('glslangValidatorPath'))
+    console.log('temp dir set to', os.tmpdir())
+
+    return {
+      glslangPath: c.get('glslangValidatorPath') as string,
+      tmpdir: os.tmpdir()
+    }
   }
 
   public dispose() {
-    if(this.diagnosticCollection != null) {
-      this.diagnosticCollection.clear()
-      this.diagnosticCollection.dispose()
-    }
+    this.diagnosticCollection.clear()
+    this.diagnosticCollection.dispose()
   }
 
   private configChange(e: vscode.ConfigurationChangeEvent) {
     if (e.affectsConfiguration('mcglsl')) {
-
+      console.log('config changed')
+      this.config = this.initConfig()
     }
   }
 
@@ -37,10 +61,14 @@ export default class GLSLProvider implements vscode.CodeActionProvider {
     this.lint(e.document)
   }
 
-  private lint(document: vscode.TextDocument) {
+  private async lint(document: vscode.TextDocument) {
     if(document.languageId !== 'glsl') {
       return
     }
+
+    fs.mkdirSync(`${this.config.tmpdir}/shaders`)
+    sym(`${vscode.workspace.rootPath}/shaders/composite.frag`, `${this.config.tmpdir}/shaders/composite.banana`)
+      .catch((err) => {console.log(err)})
   }
 
   public provideCodeActions(document: vscode.TextDocument, 
